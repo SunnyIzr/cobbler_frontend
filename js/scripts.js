bdbbdf5dfc8dbd5d32a614f9085bf9ce94df6d7b
Andrey Gorbunov <andrey@optimuspro.ru>
2015-03-28 04:04:59 +0300

    Refs #5098: Accessories - mobile version - sticky bottom bar                                          - disable slider Update Javascript code for better isolation
    --HG--
    branch : features

--- js/scripts.js
+++ js/scripts.js
@@ -1,150 +1,214 @@
-var sliderInitialized = false;
+(function ($) {
+    "use strict";
 
-$(document).on('ready', function () {
-    resizeSlide();
-    stickyProcessControls();
-
-
-    $('.faqQuestions .question').on('click', function() {
-        $(this).closest('.item').toggleClass('expanded');
-        return false;
-    });
-
-    $('.myShoes .section.orderContent .itemTypes .itemHeader').on('click', function() {
-        $(this).closest('.item').toggleClass('expanded');
-        return false;
-    });
-
-    $('.menuToggleAction').on('click', function () {
-        $('#menuOverlay').addClass('activated');
-    });
-
-    $('#menuOverlay').find('.closeButton').on('click', function () {
-        $(this).closest('#menuOverlay').removeClass('activated');
-        return false;
-    });
-
-    $('.getStarted .orderForm .hint .icon').click(function(event) {
-        event.preventDefault();
-
-        $(this).closest('.hint').toggleClass('active');
-    });
-
-    $('.getStarted .orderForm .compositeField .upDownButton').click(function(event) {
-        event.preventDefault();
-
-        var $this = $(this);
-        var control = $this.closest('.compositeField');
-
-        var valueLabel = control.find('.value');
-        var input = control.find('input');
-
-        var currentValue = parseInt(input.val());
-
-        if ($this.hasClass('increase')) {
-            currentValue += 1;
-        } else {
-            if (currentValue == 0) {
-                return false;
-            }
-
-            currentValue -= 1;
+    $(document).on('ready', function () {
+        if ($('#content.homepage').length > 0) {
+            HomePage();
+        } else if ($('#content.accessories').length > 0) {
+            AccessoriesPage();
+        } else if ($('#content.myShoes').length > 0) {
+            MyShoesPage();
+        } else if ($('#content.getStarted').length > 0) {
+            GetStartedPage();
         }
 
-        input.val(currentValue);
-        valueLabel.text(currentValue);
+        $('.faqQuestions .question').on('click', function () {
+            $(this).closest('.item').toggleClass('expanded');
+            return false;
+        });
 
-        var form = control.closest('form');
+        $('.menuToggleAction').on('click', function () {
+            $('#menuOverlay').addClass('activated');
+        });
 
-        form.submit();
+        $('#menuOverlay').find('.closeButton').on('click', function () {
+            $(this).closest('#menuOverlay').removeClass('activated');
+            return false;
+        });
+
     });
 
-    $('.getStarted .orderForm').submit(function(event) {
-        event.preventDefault();
 
-        var form = $(this);
-        var data = form.serialize();
+    function HomePage() {
+        $(window).on('resize', _updateIntroSectionSize);
+        _updateIntroSectionSize();
 
-        $.post(
-            form[0].action,
-            data
-        ).done(function(data) {
-            var orderAmount = parseInt(data);
-            form.find('.totalContainer .value .digit').text(orderAmount);
+        function _updateIntroSectionSize() {
+            var viewportWidth = $(window).width();
+            var viewportHeight = $(window).height();
 
-            var processControls = $('#content').find('.processControls');
-            if (orderAmount > 0) {
-                processControls.slideDown('slow');
+            var imageWidth = 1600;
+            var imageHeight = 889;
+
+            var imageCoef = imageWidth / imageHeight;
+
+            if (viewportWidth / viewportHeight > imageCoef) {
+                $('.homepage .section.intro').height(viewportWidth / imageCoef);
             } else {
-                processControls.slideUp('slow');
+                //$('.homepage .section.intro').height('');
+                $('.homepage .section.intro').height(Math.min(imageHeight, viewportHeight * 0.9));
             }
-        }).fail(function () {
-            alert('Whoops! It looks, that we have some issues on our side. Please, try again later or contact the support.');
-        });
-    });
-});
-
-$(window).on('resize', function () {
-    resizeSlide();
-});
-
-$(window).on('scroll', function () {
-    stickyProcessControls();
-});
-
-
-function resizeSlide () {
-    var viewportWidth = $(window).width();
-    var viewportHeight = $(window).height();
-
-    var imageWidth  = 1600;
-    var imageHeight = 889;
-
-    var imageCoef = imageWidth / imageHeight;
-
-    if (viewportWidth / viewportHeight > imageCoef) {
-        $('.homepage .section.intro').height(viewportWidth / imageCoef);
-    } else {
-        //$('.homepage .section.intro').height('');
-        $('.homepage .section.intro').height(Math.min(imageHeight, viewportHeight * 0.9));
-    }
-
-    if (Foundation.utils.is_large_up()) {
-        if (!sliderInitialized) {
-            $('#content.accessories').find('.section.accessories .itemsPages').slick({
-                dots: false,
-                arrows: true
-            });
-            sliderInitialized = true;
-        }
-    } else {
-        if (sliderInitialized) {
-            $('#content.accessories').find('.section.accessories .itemsPages').slick('unslick');
-            sliderInitialized = false;
         }
     }
 
+    function GetStartedPage() {
+        $('.orderForm .hint .icon').click(function (event) {
+            event.preventDefault();
 
+            $(this).closest('.hint').toggleClass('active');
+        });
 
+        $('.orderForm .compositeField .upDownButton').click(function (event) {
+            event.preventDefault();
 
+            var $this = $(this);
+            var control = $this.closest('.compositeField');
 
-}
+            var valueLabel = control.find('.value');
+            var input = control.find('input');
 
+            var currentValue = parseInt(input.val());
 
-function stickyProcessControls() {
-    var footer = $("#content.accessories").find(".processControls");
+            if ($this.hasClass('increase')) {
+                currentValue += 1;
+            } else {
+                if (currentValue == 0) {
+                    return false;
+                }
 
-    var pos = footer.position();
-    var height = $(window).height();
-    height = height - pos.top;
-    height = height - footer.height();
+                currentValue -= 1;
+            }
 
+            input.val(currentValue);
+            valueLabel.text(currentValue);
 
-    if (height > 0) {
-        footer.addClass('sticky');
-        footer.css({
-            'position': 'fixed',
-            'bottom' : '0'
+            var form = control.closest('form');
+
+            form.submit();
+        });
+
+        $('.orderForm').submit(function (event) {
+            event.preventDefault();
+
+            var form = $(this);
+            var data = form.serialize();
+
+            $.post(
+                form[0].action,
+                data
+            ).done(function (data) {
+                    var orderAmount = parseInt(data);
+                    form.find('.totalContainer .value .digit').text(orderAmount);
+
+                    var processControls = $('#content').find('.processControls');
+                    if (orderAmount > 0) {
+                        processControls.slideDown('slow');
+                    } else {
+                        processControls.slideUp('slow');
+                    }
+                }).fail(function () {
+                    alert('Whoops! It looks, that we have some issues on our side. Please, try again later or contact the support.');
+                });
         });
     }
-}
+
+
+    function MyShoesPage() {
+        $('.section.orderContent .itemTypes .itemHeader').on('click', function () {
+            $(this).closest('.item').toggleClass('expanded');
+            return false;
+        });
+    }
+
+    function AccessoriesPage() {
+        SliderWrapper();
+        StickyProcessControls();
+
+
+        function SliderWrapper() {
+            var _sliderInitialized = false;
+
+            $(window).on('resize', _onResize);
+            _onResize();
+
+            function _onResize() {
+                if (Foundation.utils.is_large_up()) {
+                    if (!_sliderInitialized) {
+                        $('#content.accessories').find('.section.accessories .itemsPages').slick({
+                            dots: false,
+                            arrows: true
+                        });
+                        _sliderInitialized = true;
+                    }
+                } else {
+                    if (_sliderInitialized) {
+                        $('#content.accessories').find('.section.accessories .itemsPages').slick('unslick');
+                        _sliderInitialized = false;
+                    }
+                }
+            }
+        }
+
+        function StickyProcessControls() {
+            var _bar = $("#content.accessories").find(".processControls");
+            var _topThreshold = 0;
+            var _bottomThreshold = 0;
+            var _document = $(document);
+            var _isVisible = false;
+            var _isEnabled = false;
+
+            $(window).on('resize', _onResize);
+            _onResize();
+
+            function _onResize() {
+                if (Foundation.utils.is_large_up()) {
+                    if (_isEnabled) {
+                        _isEnabled = false;
+                        $(window).off('scroll', _updateBarState);
+                        _bar.removeClass('sticky');
+                        _bar.show();
+                    }
+                } else {
+                    if (!_isEnabled) {
+                        _calcThresholds();
+                        $(window).on('scroll', _updateBarState);
+                        _isEnabled = true;
+                    }
+                }
+            }
+
+            function _calcThresholds() {
+                var footer = $('#footer');
+                var documentHeight = $(document).height();
+                var viewportHeight = $(window).height();
+
+                var pos = _document.scrollTop();
+
+                _topThreshold = viewportHeight * 0.2;
+                _bottomThreshold = documentHeight - footer.outerHeight() - viewportHeight;
+
+                _isVisible = pos > _topThreshold;
+            }
+
+            function _updateBarState() {
+                var pos = _document.scrollTop();
+
+                if (pos <= _bottomThreshold && pos > _topThreshold) {
+                    if (!_isVisible) {
+                        _bar.addClass('sticky').hide().slideDown();
+                        _isVisible = true;
+                    } else {
+                        _bar.addClass('sticky');
+                    }
+                } else if (pos <= _topThreshold && _isVisible) {
+                    _bar.slideUp().promise().done(function () {
+                        _bar.removeClass('sticky');
+                        _isVisible = false;
+                    });
+                } else {
+                    _bar.removeClass('sticky');
+                }
+            }
+        }
+    }
+})(jQuery);
